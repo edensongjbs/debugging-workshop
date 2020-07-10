611ac29aeb769690ba7940972c256b4820a09c15
edensongjbs <edensongjbs@gmail.com>
2020-07-10 15:08:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  
+  const username = document.getElementById('name-input')
   let joke;
 
   function fetchJoke(){
     fetch('https://icanhazdadjoke.com/', {
-      headers: {
-        "Accept": "application/json"
-      }
-    })
+      headers: {"Accept":"application/json"}}
+    )
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      joke = jokeData.joke
+      const newJokeLi = document.createElement('li')
+      newJokeLi.innerHTML = `
+      <span class="username">${username.value} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+      form.reset()
+    })
   }
 
-  form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(username.value === "") return;
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
   })
 })
